ff43fa7d25628086daaf44b0fb59cb37634f9227
Dylan7654 <dylan.kreuzinger@gmail.com>
2025-04-19 21:39:03 +0200

    Alle Rumpf Texturen neu.

--- strings.lua
+++ strings.lua
@@ -87,6 +87,8 @@ return {
         ["br218_or2_name"] = 	"BR 218 orientrot DB AG (1994)",
         ["br218_vr_name"] = 	"BR 218 verkehrsrot DB AG (1998)",
         ["br218_vr2_name"] = 	"BR 218 verkehrsrot DB AG 2 (1998)",
+        ["br218_vr3_name"] = 	"BR 218 verkehrsrot DB AG Nah.sh (2014)",
+        ["br218_vr4_name"] = 	"BR 218 verkehrsrot DB AG 2 (1998)",
 		
 		["218_desc"] = "Die G 1206 ist eine dieselhydraulische Lokomotive für den schweren Rangier- und Streckendienst. Die Lok wurde 1997 von MaK (Maschinenbau Kiel) entwickelt und gebaut. 1998 wurde MaK von Vossloh aufgekauft, seitdem laufen die Loks unter dem Namen Vossloh G 1206.",
 
