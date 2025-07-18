fbe06d5965259e6e6e4c973685bbd9e36fa711bc
Florian Thiel <flth@flth.de>
2025-07-18 16:55:04 +0200

    Englische Übersetzung

--- strings.lua
+++ strings.lua
@@ -2,15 +2,202 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "Mak G1206",
-		["mod_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by MaK (Maschinenbau Kiel). In 1998 MaK was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+				["mod_name"] = "Class 210 | Class 215 | Class 216 | Class 217 | Class 218 | Class 219 (\"V 160-Familie\")",
+		["mod_desc"] = "In the post-war period, the Bundesbahn planned a new construction programme for single-engine large diesel locomotives for medium-distance service. The first pre-series machine was put into operation in 1956, series production began in 1964 and the locomotives were used for long-distance, local and freight transport. The V 160 family includes the 210, 215, 216 (V 160), 217 (V 162), 218, 219 (V 169) and the 218.8 225 substructure series. Some series proved more successful than others, but overall this family series characterised the image of the German Federal Railways' mainline diesel locomotives. Many locomotives are still in service with various companies today.",
 
-        ["g1206_menu"] = "Mak G1206",
-        ["g1206_fake_menu"] = "Mak G1206 Fakes",
-		["g1206_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by MaK (Maschinenbau Kiel). In 1998 MaK was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+        ["br210_menu"] = "DB Class 210",
+        ["br215_menu"] = "DB Class 215",
+        ["br216_menu"] = "DB Class 216 (V 160)",
+        ["br217_menu"] = "DB Class 217 (V 162)",
+        ["br218_menu"] = "DB Class 218",
+        ["br219_menu"] = "DB Class 219 (V 169)",
+        ["dh1504_menu"] = "DH 1504 (ex Class 216)",
 
-		--- Standard ---
-        ["g1206_alpha_name"] = "G 1206 Alpha Trains (2017)",
+        ["br210_fake_menu"] = "DB Class 210 Fakes",
+        ["br215_fake_menu"] = "DB Class 215 Fakes",
+        ["br216_fake_menu"] = "DB Class 216 (V160) Fakes",
+        ["br217_fake_menu"] = "DB Class 217 (V162) Fakes",
+        ["br218_fake_menu"] = "DB Class 218 Fakes",
+        ["br219_fake_menu"] = "DB Class 219 (V169) Fakes",
+        ["dh1504_menu"] = "DH 1504 (ex Class 216) Fakes",
+
+        ['fake'] = 'Fake - ',
+
+        ["v160_familie_fake"] = "Active fake engines?",
+        ["option_fake_v160_familie_desc"] = "Active the fake engines (no light, no driver)?",
+
+		["v160_familie_privatbahn"] = "Activate private liveries?",
+		["option_privatbahn_v160_familie_desc"] = "Should the private liveries of all classes are activated in the game?",
+
+        ["v160_familie_br210"] = "Activate Class 210?",
+        ["option_br210_v160_familie_desc"] = "Should the Class 210 be activated in the game?",
+
+        ["v160_familie_br215"] = "Activate Class 215?",
+        ["option_br215_v160_familie_desc"] = "Should the Class 215 be activated in the game?",
+
+        ["v160_familie_br216"] = "Activate Class 216?",
+        ["option_br216_v160_familie_desc"] = "Should the Class 216 be activated in the game?",
+
+        ["v160_familie_br217"] = "Activate Class 217?",
+        ["option_br217_v160_familie_desc"] = "Should the Class 217 be activated in the game?",
+
+        ["v160_familie_br218"] = "Activate Class 218?",
+        ["option_br218_v160_familie_desc"] = "Should the Class 218 be activated in the game?",
+
+        ["v160_familie_br219"] = "Activate Class 219?",
+        ["option_br219_v160_familie_desc"] = "Should the Class 219 be activated in the game?",
+
+        ["v160_familie_br225"] = "Activate Class 225?",
+        ["option_br225_v160_familie_desc"] = "Should the Class 225 be activated in the game?",
+
+        ["v160_familie_br226"] = "Activate Class 226?",
+        ["option_br226_v160_familie_desc"] = "Should the Class 226 be activated in the game?",
+
+        ["v160_familie_br226"] = "Activate DH 1504?",
+        ["option_br226_v160_familie_desc"] = "Should the DH 1504  be activated in the game?",
+
+        ["jrm_param_type_tooltip"] = "Engines of the V 160 family",
+
+        ["motor_mb_839"] = 		" This locomotive is equipped with the Mercedes-Benz 839 Bb engine.",
+        ["motor_tb10_956"] = 	" This locomotive is equipped with the MTU MA 12 V 956 TB10 engine.",
+        ["motor_pa_4v"] = 		" This locomotive is equipped with the Pielstick 16 PA 4V 200 engine.",
+        ["motor_tb11_956"] = 	" This locomotive is equipped with the MTU MA 12 V 956 TB11 engine.",
+        ["motor_tb11_956_neu"] = " This locomotive is equipped with the MTU MA 12 V 956 TB11 (exhaust-optimised) engine.",
+        ["motor_r40"] = 		" This locomotive is equipped with the MTU 16 V4000 R40 engine.",
+
+        ["br210_ar_name"] = 	"Class 210 crimson red DB (1970)",
+        ["br210_ob_name"] = 	"Class 210 turquoise/cream DB (1974)",
+
+		["210_desc"] = "The Class 210 was built in 8 units from 1970 in order to obtain a more powerful version of the Class 218. After the successful test of the Class 219, the decision was made in favour of a more powerful turbine as a booster. The series fulfilled the expectations placed in it, but the heating power in the Windert was too weak, so that long trains often had to be run in double traction with a Class 218. From 1978 onwards, the turbines were increasingly damaged because they were started too often during operation, which significantly reduced their service life. In 1979, the gas turbines were removed from all locomotives and all locomotives were classified as sub-series 218.9.",
+
+        ["br215_ar_name"] = 	"Class 215 crimson red DB (1968)",
+        ["br215_ar2_name"] = 	"Class 215 crimson red 2 DB (1968)",
+        ["br215_ob_name"] = 	"Class 215 turquoise/cream DB (1974)",
+        ["br215_ob2_name"] = 	"Class 215 turquoise/cream DB AG (1994)",
+        ["br215_or_name"] = 	"Class 215 orient red DB (1987)",
+        ["br215_or2_name"] = 	"Class 215 orient red DB AG (1994)",
+        ["br215_vr_name"] = 	"Class 215 traffic red DB AG (1998)",
+        ["br215_vr2_name"] = 	"Class 215.9 traffic red DB Autozug Sylt (2003)",
+
+        ["br225_ob_name"] = 	"Class 225 turquoise/cream DB AG (2001)",
+        ["br225_or_name"] = 	"Class 225 orient red DB AG (2001)",
+        ["br225_vg_name"] = 	"Class 225 traffic yellow DB Bahnbau Gruppe (2014)",
+        ["br225_vr_name"] = 	"Class 225 traffic red DB AG (2001)",
+        ["br225_vr2_name"] = 	"Class 225 traffic red Railion (2003)",
+        ["br225_vr3_name"] = 	"Class 225 traffic red Railion Logistics (2007)",
+
+		["215_desc"] = "The main difference between the class 215 and the other related series of the V 160 family was the steam heating boiler. It was planned as the V 163, but only came into service from 1968 and was intended to replace the steam locomotives with steam heating in front of passenger trains, as the Class 218 only had electric train heating. The locomotives were used in front of all types of trains and were increasingly taken out of service from the year 200. In 2003, 17 locomotives came to DB AutoZug and were rebuilt analogue to the Class 225 and operated as 215.9. The service ended in 2019.",
+
+        ["225_desc"] = "The class 225 was created by redesigning 68 locomotives of the class 215 from 2001 onwards. The steam heating systems were removed immediately or later. They still have double traction control so that they can run together with other locomotives of the class. Some locomotives were sold to private companies.",
+
+        ["v160_ar_name"] = 		"V 160 crimson red DB (1964)",
+        ["v160_ar2_name"] = 	"V 160 crimson red 2 DB (1964)",
+        ["br216_ar_name"] = 	"Class 216 crimson red DB (1968)",
+        ["br216_ar2_name"] = 	"Class 216 crimson red 2 DB (1968)",
+        ["br216_ob_name"] = 	"Class 216 turquoise/cream DB (1974)",
+        ["br216_ob2_name"] = 	"Class 216 turquoise/cream Dach turquoise DB (1974)",
+        ["br216_or_name"] = 	"Class 216 orient red DB (1987)",
+        ["br216_or2_name"] = 	"Class 216 orient red DB AG (1994)",
+        ["br216_vr_name"] = 	"Class 216 traffic red DB AG (1998)",
+        ["br216_vr2_name"] = 	"Class 216 traffic red DB Cargo (2003)",
+
+        ["v160-010_ar_name"] =  "V 160 010 crimson red DB, Pre-series (1960)",
+        ["br216-010_ar_name"] =  "Class 216 010 crimson red DB, Pre-series (1968)",
+
+        ["br226_vr_name"] = 	"Class 226 traffic red xxxx (2003)",
+
+		["216_desc"] = "The V 160, later the Class 216, was the first member of the family of the same name. The first ten pre-series machines were delivered in 1960 and nine of them still had a round head shape (called ‘Lollo’). The tenth machine was given a square locomotive body, which was later adopted for the series machine. The locomotives proved their worth, the pre-series locomotives were taken out of service by 1984, in the absence of passenger coaches with steam heating, the Class 216 was used by DB Cargo from 1998, all machines were decommissioned by 2004.",
+
+        ["dh1504_niag_8_name"] = "DH 1504 NIAG 8 (2000)",
+        ["dh1504_niag_9_name"] = "DH 1504 NIAG 9 (2000)",
+        ["dh1504_mkb_name"] = "DH 1504 MKB (1998)",
+        ["dh1504_mkb_silber_name"] = "DH 1504 MKB silver (2007)",
+        ["dh1504_ohe_name"] = "DH 1504 OHE (2000)",
+        ["dh1504_blc_name"] = "DH 1504 BLC (2016)",
+
+        ["dh1504_desc"] = "From 1998, the company On Rail rebuilt a total of 6 locomotives to the DH 1504, which was an almost completely new locomotive, only the locomotive body and the bogies were largely taken over. The locomotives were fitted with wide steps at both ends and radio remote control for shunting tasks.",
+
+        ["v162_ar_name"] = 		"V 162 crimson red DB (1965)",
+        ["br217_ar_name"] = 	"Class 217 crimson red DB (1968)",
+        ["br217_ob_name"] = 	"Class 217 turquoise/cream DB (1974)",
+        ["br217_ob2_name"] = 	"Class 217 turquoise/cream DB AG (1994)",
+        ["br217_or_name"] = 	"Class 217 orient red DB (1987)",
+        ["br217_or2_name"] = 	"Class 217 orient red DB AG (1994)",
+        ["br217_vr_name"] = 	"Class 217 traffic red DB AG (1998)",
+
+        ["br753_ob_name"] = 	"Class 753 turquoise/cream DB (1989)",
+        ["br753_ob2_name"] = 	"Class 753 turquoise/cream DB AG (1994)",
+        ["br753_pb_name"] = 	"Class 753 crimson red/cream DB AG (2003)",
+
+		["217_desc"] = "In contrast to the class 215, the V162 (designated the class 217 from 1968) was ordered with an electric train heating system. It was equipped with an additional auxiliary motor, which was also intended to provide traction power in freight transport. However, the Class 218s delivered shortly afterwards were more economical, so that no more new Class 217s were procured. From 1997 onwards, the locomotives were transferred to DB Cargo and were all decommissioned by the end of 2011.",
+
+        ["br218_ar_name"] = 	"Class 218 crimson red DB (1971)",
+        ["br218_ar2_name"] = 	"Class 218 DB crimson red 2 DB (1971)",
+        ["br218_ar3_name"] = 	"Class 218 DB AG crimson red DB AG (1994)",
+        ["br218_ar4_name"] = 	"Class 218 DB crimson red DB - Pielstick (1973)",
+        ["br218_ar5_name"] = 	"Class 218 DB crimson red DB - MTU4000 R40/R41 (1973)",
+
+        ["br218_ob_name"] = 	"Class 218 turquoise/cream DB (1974)",
+        ["br218_ob2_name"] = 	"Class 218 turquoise/cream Dach turquoise DB (1974)",
+        ["br218_ob3_name"] = 	"Class 218 turquoise/cream Dach turquoise DB (1975)",
+        ["br218_ob4_name"] = 	"Class 218 turquoise/cream DB AG (1994)",
+        ["br218_ob5_name"] = 	"Class 218 turquoise/cream 2 DB AG (1994)",
+        ["br218_ob6_name"] = 	"Class 218 turquoise/cream Dach turquoise DB AG (2006)",
+        ["br218_ob7_name"] = 	"Class 218 turquoise/cream Dach turquoise DB AG (2025)",
+        ["br218_ob8_name"] = 	"Class 218 turquoise/cream DB - Pielstick (1974)",
+        ["br218_ob9_name"] = 	"Class 218 turquoise/cream DB - MTU4000 R40/R41 (1994)",
+        ["br218_ob10_name"] = 	"Class 218 turquoise/cream DB AG - MTU4000 R40/R41 (1994)",
+
+        ["br218_pb_name"] = 	"Class 218 crimson red/cream DB (1976)",
+        ["br218_citybahn_name"] = "Class 218 pebble grey/orange DB (1984)",
+        ["br218_citybahn2_name"] = "Class 218 pebble grey/orange DB AG (1994)",
+        ["br218_or_name"] = 	"Class 218 orient red DB (1987)",
+        ["br218_or2_name"] = 	"Class 218 orient red DB AG (1994)",
+        ["br218_or3_name"] = 	"Class 218 orient red DB AG Autozug Sylt (2003)",
+        ["br218_or5_name"] = 	"Class 218 orient red DB - Pielstick (1987)",
+        ["br218_or6_name"] = 	"Class 218 orient red DB AG - Pielstick (1994)",
+        ["br218_or7_name"] = 	"Class 218 orient red DB - MTU4000 R40/R41 (1987)",
+        ["br218_or8_name"] = 	"Class 218 orient red DB AG - MTU4000 R40/R41 (1994)",
+
+        ["br218_or_versuch_name"] = "Class 218 orient red test livery DB (1987)",
+        ["br218_vr_name"] = 	"Class 218 traffic red DB AG (1998)",
+        ["br218_vr2_name"] = 	"Class 218 traffic red 2 DB AG (1998)",
+        ["br218_vr3_name"] = 	"Class 218 traffic red DB AG Nah.sh (2014)",
+        ["br218_vr4_name"] = 	"Class 218 traffic red DB AG 3 Löwen Takt (2000)",
+        ["br218_vr5_name"] = 	"Class 218 traffic red DB Autozug Sylt (2003)",
+        ["br218_vr6_name"] = 	"Class 218 traffic red DB AG bwegt (2017)",
+        ["br218_vr7_name"] = 	"Class 218 traffic red DB AG - TB10 (1998)",
+        ["br218_vr8_name"] = 	"Class 218 traffic red DB AG Kempten (1998)",
+        ["br218_vr10_name"] = 	"Class 218 traffic red DB AG - Pielstick (1998)",
+        ["br218_vr11_name"] = 	"Class 218 traffic red DB AG - MTU4000 R40/R41 (1998)",
+
+
+        ["br218.8_vr_name"] = 	"Class 218.8 traffic red Abschlepplok DB AG (2004)",
+        ["br218_vr9_name"] = 	"Class 218 440 traffic red Südostbayernbahn (2013)",
+        ["br218_sbahn_ts_name"] = "Class 218 486 traffic red DB AG S-Bahn Stuttgart (2015)",
+        ["br218_west_name"] = 	"Class 218 460 turquoise/cream DB AG Westfrankenbahn (2016)",
+        ["br218_nesa2_name"] = 	"Class 218 155 crimson red NeSa (2018)",
+        ["br218_nesa_name"] = 	"Class 218 105 crimson red/cream NeSa (2019)",
+        ["br218_ris_name"] = 	"Class 218 468 tiefschwarz/grau RIS (2019)",
+        ["br218_press_name"] = 	"Class 218 traffic blue Pressnitztalbahn (2020)",
+        ["br218_khr_name"] = 	"Class 218 117 pebble grey/orange Kübler Heavy Rail (2021)",
+        ["br218_press2_name"] = "Class 218 traffic blue Pressnitztalbahn (2022)",
+        ["br218_ueg_name"] = 	"Class 218 333 mint turquoise UEG (2022)",
+        ["br218_or4_name"] = 	"Class 218 406 orient red DB AG (2023)",
+        ["br218_ice_name"] = 	"Class 218 341 ICE design DB AG (2023)",
+        ["br218_bke_name"] = 	"Class 218 476 turquoise/cream BKE (2024)",
+        ["br218_fzi_name"] =    "Class 218 497 Märklin advertising (2021)",
+
+		["218_desc"] = "The class 218 is a four-axle diesel locomotive and for many years it was the most common and most important representative of West German mainline diesel locomotives. As the last member of the V 160 locomotive family to be developed, it was more or less the concentrate of all the previous series and has proved itself accordingly to this day. The locomotives were used in both passenger and goods train services. In the course of its service life, it has seen all the different liveries of the Deutsche Bundesbahn and Deutsche Bahn AG. Some locomotives are still in service with DB or private companies today.",
+
+        ["v169_ar_name"] = 		"V 169 crimson red DB (1965)",
+        ["br219_ar_name"] = 	"Class 219 crimson red DB (1968)",
+        ["br219_ar2_name"] = 	"Class 219 crimson red DB (1974)",
+        ["br219_bwg_name"] = 	"Class 219 Waldhof AG (1998)",
+        ["br219_evb_name"] = 	"Class 219 EVB (2001)",
+        ["br219_lokrapid_name"] = 	"Class 219 ruby red Lokrapid (2022)",
+
+		["219_desc"] = "The V169 was built as a prototype for locomotives with a gas turbine as auxiliary drive. From 1968 it was categorised as class 219. In 1974, the gas turbine was finally removed following damage, as was the heating system. After that, it was only used as a freight locomotive and was decommissioned in 1978.\nAfter a 20-year stay in Italy, the locomotive was brought back to Germany in 1998 and was used as DH 280.01 by the railway company Waldhof AG. In 2001, it was sold to Eisenbahnen und Verkehrsbetriebe Elbe-Weser (EVB), where it was used in front of container trains between Hamburg, Bremerhaven and Bremen. Following an accident in 2013, the locomotive was once again decommissioned. In 2021, it was sold to LokRapid in Oldenburg, refurbished by the self-employed locomotive driver Roland Sandkuhl and has been back in service in front of goods trains since a main inspection was completed in September 2022.",
 
 
 	},
@@ -200,6 +387,7 @@ return {
         ["br218_or4_name"] = 	"BR 218 406 orientrot DB AG (2023)",
         ["br218_ice_name"] = 	"BR 218 341 ICE-Design DB AG (2023)",
         ["br218_bke_name"] = 	"BR 218 476 ozeanblau/beige BKE (2024)",
+        ["br218_fzi_name"] =    "BR 218 497 Märklin Werbung (2021)",
 
 		["218_desc"] = "Die BR 218 ist eine vierachsige Diesellok und über viele Jahre hinweg war sie die häufigste und wichtigste Vertreterin der westdeutschen Streckendiesellokomotiven. Als zuletzt entwickeltes Mitglied der V 160-Lokfamilie war sie quasi das Konzentrat aus allen vorherigen Baureihen und dementsprechend bewährt hat sie sich bis heute. Die Loks wurden im Reise- und auch im Güterzugdienst eingesetzt. Im Laufe ihrer Dienstzeit hat sie alle Lackierungsvarianten der Deutschen Bundesbahn und Deutschen Bahn AG mitgemacht. Einige Loks stehen auch heute noch im Dienst bei der DB oder privaten Unternehmen.",
 
